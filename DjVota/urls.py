ece59eebd039326bdd1adc8280e80880e1cda893
GenisTorresDonaire <gtorresdonaire@iesesteveterradas.cat>
2018-02-14 20:26:08 +0100

    final sin 6

--- DjVota/urls.py
+++ DjVota/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.urls import include, path
 
 urlpatterns = [
-    url(r'^app/', include('app.urls')),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^app/', include('app.urls')),
+    #url(r'^admin/', admin.site.urls),
+    path('app/', include('app.urls')),
+    path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 
-from .models import Opciones, Preguntas
+from .models import Opciones, Preguntas, Voto
 
 
 class IndexView(generic.ListView):
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Preguntas.objects.order_by('-pub_date')[:5]
+        return Preguntas.objects.order_by('fechaFinal_pregunta')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -28,7 +28,7 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     preguntas = get_object_or_404(Preguntas, pk=question_id)
     try:
-        selected_choice = preguntas.choice_set.get(pk=request.POST['choice'])
+        seleccionOpcion = preguntas.opciones_set.get(pk=request.POST['opcion'])
     except (KeyError, Opciones.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'app/detail.html', {
@@ -36,8 +36,11 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        #seleccionOpcion.votes += 1
+        v = Voto()
+        v.id_opciones = seleccionOpcion
+
+        v.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
